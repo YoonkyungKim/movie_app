fe1608ebff2e1e89385ee1f495012ffb679bdfc3
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2020-05-11 22:14:44 -0400

    Updates

---
+++ precache-manifest.d80cb0e146f0dc313c9d36e18a75840c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb51af7d8e71161f399d79a9ce8d5fa",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "cfb079251569ad01076d",
+    "url": "/movie_app/static/css/main.55a5a5fe.chunk.css"
+  },
+  {
+    "revision": "1081af6c48b0570cc191",
+    "url": "/movie_app/static/js/2.1c0027d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.1c0027d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb079251569ad01076d",
+    "url": "/movie_app/static/js/main.db22b39a.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
